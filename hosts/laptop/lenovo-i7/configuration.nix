a4113846f19590957510a0dab3686e9e54814ab1
Togo-GT <michael.kaare.nielsen@gmail.com>
2025-09-13 23:28:12 +0200

    test build 1

--- hosts/laptop/lenovo-i7/configuration.nix
+++ hosts/laptop/lenovo-i7/configuration.nix
@@ -1,23 +1,21 @@
-{  ... }:
+{ flakeRoot, ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
-    ../../modules/system/boot.nix
-    ../../modules/system/networking.nix
-    ../../modules/system/locale.nix
-    ../../modules/system/audio.nix
-    ../../modules/desktop/xserver.nix
-    ../../modules/desktop/display-manager.nix
-    ../../modules/desktop/plasma.nix
-    ../../modules/security/ssh.nix
-    ../../modules/security/sudo.nix
-    ../../modules/security/firewall.nix
-    ../../profiles/base.nix
-    ../../profiles/desktop.nix
-    ../../users/gt/default.nix
-    ../../system/stateversion.nix
+    (import (flakeRoot + "/modules/system/boot.nix"))
+    (import (flakeRoot + "/modules/system/networking.nix"))
+    (import (flakeRoot + "/modules/system/locale.nix"))
+    (import (flakeRoot + "/modules/system/audio.nix"))
+    (import (flakeRoot + "/modules/desktop/xserver.nix"))
+    (import (flakeRoot + "/modules/desktop/display-manager.nix"))
+    (import (flakeRoot + "/modules/desktop/plasma.nix"))
+    (import (flakeRoot + "/modules/security/ssh.nix"))
+    (import (flakeRoot + "/modules/security/sudo.nix"))
+    (import (flakeRoot + "/modules/security/firewall.nix"))
+    (import (flakeRoot + "/profiles/base.nix"))
+    (import (flakeRoot + "/profiles/desktop.nix"))
+    (import (flakeRoot + "/users/gt/default.nix"))
+    (import (flakeRoot + "/system/stateversion.nix"))
   ];
 }
-
-
