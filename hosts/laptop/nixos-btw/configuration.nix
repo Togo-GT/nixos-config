abe35e5f3387160bb7eb46d06ea7afe96e7afdce
Togo-GT <michael.kaare.nielsen@gmail.com>
2025-09-14 11:38:53 +0200

    w

--- hosts/laptop/nixos-btw/configuration.nix
+++ hosts/laptop/nixos-btw/configuration.nix
@@ -3,20 +3,20 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ../../modules/system/boot.nix
-    ../../modules/system/networking.nix
-    ../../modules/system/locale.nix
-    ../../modules/system/audio.nix
-    ../../modules/desktop/xserver.nix
-    ../../modules/desktop/display-manager.nix
-    ../../modules/desktop/plasma.nix
-    ../../modules/security/ssh.nix
-    ../../modules/security/sudo.nix
-    ../../modules/security/firewall.nix
-    ../../profiles/base.nix
-    ../../profiles/desktop.nix
-    ../../modules/system/rio.nix
-    ../../users/gt/gt.nix
-    ../../modules/system/stateversion.nix
+    ../../../modules/system/boot.nix
+    ../../../modules/system/networking.nix
+    ../../../modules/system/locale.nix
+    ../../../modules/system/audio.nix
+    ../../../modules/desktop/xserver.nix
+    ../../../modules/desktop/display-manager.nix
+    ../../../modules/desktop/plasma.nix
+    ../../../modules/security/ssh.nix
+    ../../../modules/security/sudo.nix
+    ../../../modules/security/firewall.nix
+    ../../../profiles/base.nix
+    ../../../profiles/desktop.nix
+    ../../../modules/system/rio.nix
+    ../../../users/gt/gt.nix
+    ../../../modules/system/stateversion.nix
   ];
 }
